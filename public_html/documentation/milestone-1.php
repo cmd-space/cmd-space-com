8ebf597e4b3376ac4517192246052ec9dede85fd
Mason Crane <masonecrane@gmail.com>
2017-01-21 15:39:13 -0700

    created css directory and main.css for later use. Also started content for milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>cmd-space milestone 1</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<h1>Milestone 1</h1>
+
+		<h2>if (Purpose && Audience && Goal) {</h2>
+
+		<h3>Purpose:</h3>
+
+		<p>
+			My single page personal website will be used to show off my skill and talent as a web developer, and will eventually turn into a wide range of portfolio items
+		</p>
+
+		<h3>Audience:</h3>
+
+		<p>
+			My audience will mainly consist of potential freelance clients and potential employers and/or hiring managers
+		</p>
+
+		<h3>Goal:</h3>
+
+		<p>
+
+		</p>
+
+		<h2>}</h2>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	</body>
+</html>
