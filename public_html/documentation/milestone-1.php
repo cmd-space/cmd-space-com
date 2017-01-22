35ac8024c011ccb975df3d7ad45dd54bbb6b1352
Mason Crane <masonecrane@gmail.com>
2017-01-22 16:44:40 -0700

    css to properly align the nav and changed color of text. Also added content to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,31 +16,48 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+
+		<link rel="stylesheet" href="../css/main.css">
+
 	</head>
 	<body>
-		<h1>Milestone 1</h1>
+		<div class="site-wrapper">
 
-		<h2>if (Purpose && Audience && Goal) {</h2>
+			<div class="site-wrapper-inner">
 
-		<h3>Purpose:</h3>
+				<div class="cover-container">
 
-		<p>
-			My single page personal website will be used to show off my skill and talent as a web developer, and will eventually turn into a wide range of portfolio items
-		</p>
-
-		<h3>Audience:</h3>
-
-		<p>
-			My audience will mainly consist of potential freelance clients and potential employers and/or hiring managers
-		</p>
-
-		<h3>Goal:</h3>
-
-		<p>
-
-		</p>
-
-		<h2>}</h2>
+					<div class="masthead clearfix">
+						<div class="inner">
+							<h3 class="masthead-brand">cmd-space</h3>
+							<nav>
+								<ul class="nav masthead-nav">
+									<li><a href="https://bootcamp-coders.cnm.edu/~mcrane2/cmd-space-com/public_html/">home</a></li>
+									<li class="active"><a href="#">documentation</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<h1>Milestone 1</h1>
+					<h2>if (Purpose && Audience && Goal) {</h2>
+					<h3>Purpose:</h3>
+					<p>My single page personal website will be used to show off my skill and talent as a web developer, and will eventually turn into a wide range of portfolio items</p>
+					<h3>Audience:</h3>
+					<p>My audience will mainly consist of potential freelance clients and potential employers and/or hiring managers</p>
+					<h3>Goal:</h3>
+					<p>To acquire an internship, a full-time position and to attract freelance clients</p>
+					<h2>}</h2>
+					<h2>if (Persona == 'exists') {</h2>
+					<p>Name: Mark Zuckerberg</p>
+					<p>Age: 32</p>
+					<p>Job: Chairman and CEO of Facebook</p>
+					<p>Technology: 2017 Macbook Pro 15" running OS X 10.12.2 Sierra</p>
+					<p>Attitude: to find the right web developer to help create the newest addition to Facebook</p>
+					<p>Scenario: Mark Zuckerberg has been bombarded with resumes since posting his latest opening at Facebook. He decides to do a random search on his favorite search engine for 'best aspiring web developer of 2017' and just so happens to stumble across cmd-space.com. :D His interest is piqued when he notices that Bootstrap is poorly integrated, and the color scheme was pulled from a Bootstrap template from startbootstrap.com, which clearly has a greater grasp of CSS than this website developer. Regardless of the flaws, Zuckerberg has a strong feeling that this web developer has some potential, and with a little mentoring and guidance can become a great developer. Mark Zuckerberg then fills out his information within the provided contact form, and verifies that he is not, in fact, a robot and hits submit. Mark then crosses his fingers and hopes that this web developer, Mason Crane, will contact him soon.</p>
+					<h2>}</h2>
+				</div>
+			</div>
+		</div>
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
