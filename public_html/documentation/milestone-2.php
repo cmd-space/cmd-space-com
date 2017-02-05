4c8a3ba0547e0faf08e40f0a5e330af88470575c
Mason Crane <masonecrane@gmail.com>
2017-02-05 11:30:05 -0700

    created milestone-2.php, added a few css rules, created mockups and added them to new images directory

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Milestone 2 PWP</title>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<link rel="stylesheet" href="../css/main.css">
+	</head>
+	<body>
+		<div class="site-wrapper">
+
+			<div class="site-wrapper-inner">
+
+				<div class="cover-container">
+
+					<div class="masthead clearfix">
+						<div class="inner">
+							<h3 class="masthead-brand">cmd-space</h3>
+							<nav>
+								<ul class="nav masthead-nav">
+									<li><a href="https://bootcamp-coders.cnm.edu/~mcrane2/cmd-space-com/public_html/">home</a></li>
+									<li class="active"><a href="#">documentation</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<header>
+						<h1>Milestone 2</h1>
+					</header>
+					<main>
+						<h2>Content strategy</h2>
+						<p>This first section is optional, but if I end up doing some freelance work during DDCBC I would really like to request testimonials from my clients and showcase them at the top of my PWP. If I get any testimonials during the bootcamp I think I'll only showcase one or two, and only if they represent my ability to improve the client's business, life, etc to really get across my value to potential clients/hiring managers in the future. The next section after the optional testimonial section will most likely be a brief overview of what I offer as a web developer and designer (features) and then immediately go into the benefits that I offer as a web developer and designer. The third section will be a brief, and non-threatening 'call to action' requesting that they get in touch if they want to discuss a project.</p>
+					</main>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
